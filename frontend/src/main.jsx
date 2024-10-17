a89bb26f4301e3ebbbc045ae248324bd4dc680d5
Maazin Kazi <maazinazim@gmail.com>
2024-10-17 08:46:00 +0530

    Added the routing pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import Navbar from './Components/Navbar/Navbar.jsx'
 import './Components/Navbar/Navbar.css'
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <>
+    <StrictMode>
+        <BrowserRouter >
+            <App />
+        </BrowserRouter>
+    </StrictMode>
+    </>
 )
