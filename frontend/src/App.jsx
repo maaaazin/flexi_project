cf7388d3ef1a3d25029043e2a7a3f7f83ef5779d
Maazin Kazi <maazinazim@gmail.com>
2024-10-26 19:59:07 +0530

    routing done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
+import { Route, Routes } from "react-router-dom";
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import About from './pages/About'
+import Create from './pages/Create'
+import Drops from './pages/Drops'
+import Learn from './pages/Learn'
+import Support from './pages/Support'
+import Home from './pages/Home'
+import Stats from './pages/Stats'
+
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <div>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/drops" element={<Drops />} />
+            <Route path="/stats" element={<Stats />} />
+            <Route path="/learn" element={<Learn />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/support" element={<Support />} />
+            <Route path="/create" element={<Create />} />
+        </Routes>
+      </div>
     </>
   )
 }
