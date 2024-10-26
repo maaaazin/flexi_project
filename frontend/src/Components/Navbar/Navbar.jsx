7ce0c826d9f8ad14cef08738d8cf89c689ab8516
Maazin Kazi <maazinazim@gmail.com>
2024-10-26 17:39:20 +0530

    changed the anchor tags in navbar to links

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import logo from '../../assets/logo.png'
+import React from 'react';
+import logo from '../../assets/logo.png';
+import { Link } from "react-router-dom"; 
 
 const Navbar = () => {
   return (
     <nav>  
-        <a href="/"> <img src= {logo}  alt="KRYPTONITE" className='logo'/> </a>
+        <Link to="/"> <img src={logo} alt="KRYPTONITE" className='logo' /> </Link>
         <ul>
-            <li className='links'> <a href="/drops"> Drops </a> </li>
-            <li className='links'> <a href="/stats"> Stats </a> </li>
-            <li className='links'> <a href="/create"> Create </a> </li>
-            <li className='links'> <a href="/learn"> Learn </a> </li>
-            <li className='links'> <a href="/about"> About Us </a> </li>
-            <li className='links'> <a href="/support"> Support </a> </li>
-      </ul>
+            <li className='links'> <Link to="/drops"> Drops </Link> </li>
+            <li className='links'> <Link to="/stats"> Stats </Link> </li>
+            <li className='links'> <Link to="/create"> Create </Link> </li>
+            <li className='links'> <Link to="/learn"> Learn </Link> </li>
+            <li className='links'> <Link to="/about"> About Us </Link> </li>
+            <li className='links'> <Link to="/support"> Support </Link> </li>
+        </ul>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
